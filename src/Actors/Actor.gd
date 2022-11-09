6c236cf01f13f2ff542a149510d5b30d39d24a78
Mortivarus <58182213+Mortivarus@users.noreply.github.com>
2022-11-09 15:46:28 +0100

    Update

--- src/Actors/Actor.gd
+++ src/Actors/Actor.gd
@@ -9,4 +9,4 @@ var velocity = Vector2.ZERO
 func _physics_process(delta: float) -> void:
 	velocity.y += gravity * delta
 	velocity.y = max(velocity.y, speed.y)
-	velocity = move_and_slide(velocity)
+	
